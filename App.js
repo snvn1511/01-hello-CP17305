3450b98eb081599c4de92aa94d27da958be2b7a4
snvn1511 <spxedu@gmail.com>
2023-01-06 16:03:11 +0700

    demo comp

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DemoComp from './components/DemoComp';
+ 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{fontSize:40}}>Xin chào lớp</Text>
       <StatusBar style="auto" />
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
+      <DemoComp />
     </View>
   );
 }
---
+++ components/DemoComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DemoComp = () => {
+    return (
+        <View>
+            <Text>Dòng nội dung trong DemoComp </Text>
+        </View>
+    );
+}
+
+export default DemoComp;
